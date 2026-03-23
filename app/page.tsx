aa3228280fc411ab19818b387094c4942d09f311
sikhokele-crypto <sikhokeledikwana@gmail.com>
2026-03-23 23:14:34 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { dbConnect } from "@/lib/dbConnect";
-import Match from '@/models/Match';
+import Match from '@/lib/models/Match';
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
