80cc65d8e1f07e3e321de6ba7b37088d907b4fb4
sikhokele-crypto <sikhokeledikwana@gmail.com>
2026-03-24 21:47:59 +0200

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import dbConnect from "../../../lib/dbConnect";
-import { User } from "../../../models/user";
+import { User } from "@/lib/models/user";
 import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
