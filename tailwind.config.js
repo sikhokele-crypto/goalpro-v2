848beb180c97675c3d2c301726259fe8b77423a7
sikhokele-crypto <sikhokeledikwana@gmail.com>
2026-03-29 13:18:48 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#020617",
+      },
+    },
+  },
+  plugins: [],
+}
