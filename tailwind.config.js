ff9580ff745e8a73e803a3ba96bab7f3b06037b4
sikhokele-crypto <sikhokeledikwana@gmail.com>
2026-03-29 17:03:24 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,9 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "#020617",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
